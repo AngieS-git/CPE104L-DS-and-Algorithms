02a823c2bc3ea79e520242059e5fe3281da5c285
AngieS-git <m.angelinesamson@gmail.com>
2022-03-30 19:18:28 +0800

    added solved exercise 17-1

--- Exercise17-1/myStack.h
+++ Exercise17-1/myStack.h
@@ -1,84 +1,37 @@
 //Header file: myStack.h
 
-#ifndef H_StackType
-#define H_StackType
-
+#ifndef H_StackType 
+#define H_StackType 
+ 
 #include <iostream>
 #include <cassert>
 
 #include "stackADT.h"
 
 using namespace std;
-
+  
 template <class Type>
-class stackType: public stackADT<Type>
+class stackType
 {
+
+
 public:
-    const stackType<Type>& operator=(const stackType<Type>&);
-      //Overload the assignment operator.
-
     void initializeStack();
-      //Function to initialize the stack to an empty state.
-      //Postcondition: stackTop = 0
-
-    bool isEmptyStack() const;
-      //Function to determine whether the stack is empty.
-      //Postcondition: Returns true if the stack is empty,
-      //               otherwise returns false.
-
+    bool isEmptyStack() const;  
     bool isFullStack() const;
-      //Function to determine whether the stack is full.
-      //Postcondition: Returns true if the stack is full,
-      //               otherwise returns false.
-
-    void push(const Type& newItem);
-      //Function to add newItem to the stack.
-      //Precondition: The stack exists and is not full.
-      //Postcondition: The stack is changed and newItem
-      //               is added to the top of the stack.
-
+    void push(const Type& );
     Type top() const;
-      //Function to return the top element of the stack.
-      //Precondition: The stack exists and is not empty.
-      //Postcondition: If the stack is empty, the program
-      //               terminates; otherwise, the top element
-      //               of the stack is returned.
-
     void pop();
-      //Function to remove the top element of the stack.
-      //Precondition: The stack exists and is not empty.
-      //Postcondition: The stack is changed and the top
-      //               element is removed from the stack.
+    stackType(int stackSize = 20); 
+    ~stackType(); 
 
-    stackType(int stackSize = 100);
-      //constructor
-      //Create an array of the size stackSize to hold
-      //the stack elements. The default stack size is 100.
-      //Postcondition: The variable list contains the base
-      //               address of the array, stackTop = 0, and
-      //               maxStackSize = stackSize.
-
-    stackType(const stackType<Type>& otherStack);
-      //copy constructor
-
-    ~stackType();
-      //destructor
-      //Remove all the elements from the stack.
-      //Postcondition: The array (list) holding the stack
-      //               elements is deleted.
-
-      bool operator==(const stackType<Type>& otherStack) const;
+    bool operator==(const stackType<Type>& );
 
 private:
     int maxStackSize; //variable to store the maximum stack size
     int stackTop;     //variable to point to the top of the stack
     Type *list;       //pointer to the array that holds the
                       //stack elements
-
-    void copyStack(const stackType<Type>& otherStack);
-      //Function to make a copy of otherStack.
-      //Postcondition: A copy of otherStack is created and
-      //               assigned to this stack.
 };
 
 
@@ -106,7 +59,7 @@ void stackType<Type>::push(const Type& newItem)
 {
     if (!isFullStack())
     {
-        list[stackTop] = newItem;   //add newItem to the
+        list[stackTop] = newItem;   //add newItem to the 
                                     //top of the stack
         stackTop++; //increment stackTop
     }
@@ -117,10 +70,10 @@ void stackType<Type>::push(const Type& newItem)
 template <class Type>
 Type stackType<Type>::top() const
 {
-    assert(stackTop != 0);          //if stack is empty,
+    assert(stackTop != 0);          //if stack is empty, 
                                     //terminate the program
     return list[stackTop - 1];      //return the element of the
-                                    //stack indicated by
+                                    //stack indicated by 
                                     //stackTop - 1
 }//end top
 
@@ -128,13 +81,13 @@ template <class Type>
 void stackType<Type>::pop()
 {
     if (!isEmptyStack())
-        stackTop--;                 //decrement stackTop
+        stackTop--;                 //decrement stackTop 
     else
         cout << "Cannot remove from an empty stack." << endl;
 }//end pop
 
 template <class Type>
-stackType<Type>::stackType(int stackSize)
+stackType<Type>::stackType(int stackSize) 
 {
     if (stackSize <= 0)
     {
@@ -142,10 +95,10 @@ stackType<Type>::stackType(int stackSize)
              << "be positive." << endl;
         cout << "Creating an array of size 100." << endl;
 
-        maxStackSize = 100;
+        stackSize = 10;
     }
     else
-        maxStackSize = stackSize;   //set the stack size to
+        maxStackSize = stackSize;   //set the stack size to 
                                     //the value specified by
                                     //the parameter stackSize
 
@@ -157,41 +110,22 @@ stackType<Type>::stackType(int stackSize)
 template <class Type>
 stackType<Type>::~stackType() //destructor
 {
-    delete [] list; //deallocate the memory occupied
+    delete [] list; //deallocate the memory occupied 
                     //by the array
 }//end destructor
 
 template <class Type>
-void stackType<Type>::copyStack(const stackType<Type>& otherStack)
+bool stackType<Type>:: operator==(const stackType<Type>& right)
 {
-    delete [] list;
-    maxStackSize = otherStack.maxStackSize;
-    stackTop = otherStack.stackTop;
-
-    list = new Type[maxStackSize];
-
-        //copy otherStack into this stack
-    for (int j = 0; j < stackTop; j++)
-        list[j] = otherStack.list[j];
-} //end copyStack
-
-
-template <class Type>
-stackType<Type>::stackType(const stackType<Type>& otherStack)
-{
-    list = nullptr;
-
-    copyStack(otherStack);
-}//end copy constructor
-
-template <class Type>
-const stackType<Type>& stackType<Type>::operator=
-   					(const stackType<Type>& otherStack)
-{
-    if (this != &otherStack) //avoid self-copy
-        copyStack(otherStack);
-
-    return *this;
-} //end operator=
+    if(this->stackTop!=right.stackTop)
+        return false;
+    
+    for(int i = 0; i < stackTop; i++  )
+    {
+        if(this-> list[i] != right.list [i])
+            return false;
+    }
+    return true;
+}
 
 #endif
